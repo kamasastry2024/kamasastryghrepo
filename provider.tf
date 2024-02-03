3d19833e7096fb99577878ee3e73d0618a0e61e9
kamasastry2024 <158695588+kamasastry2024@users.noreply.github.com>
2024-02-03 19:43:26 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,3 +12,5 @@ provider "azurerm" {
   subscription_id = "e495d880-46b4-4223-81c2-6753ed66b43a"
   tenant_id       = "ff355289-721e-4dd7-a663-afec62ab9d54"
 }
+
+data "azurerm_client_config" "current" {}
