5873166b5fe52beef8beb165ca2c0c3f0e660c6f
kamasastry2024 <158695588+kamasastry2024@users.noreply.github.com>
2024-02-03 19:48:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,8 +9,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  ARM_SUBSCRIPTION_ID = "e495d880-46b4-4223-81c2-6753ed66b43a"
-  ARM_TENANT_ID       = "ff355289-721e-4dd7-a663-afec62ab9d54"
 }
 
 data "azurerm_client_config" "current" {}
