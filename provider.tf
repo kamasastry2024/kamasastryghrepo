b88cc32d5063b8d14977e9708c9c3704bd22d14d
Kamasastry Kodukulla <Kamasastry.10713580@ltimindtree.com>
2024-02-03 22:53:14 +0530

    given access key

--- provider.tf
+++ provider.tf
@@ -10,9 +10,7 @@ terraform {
     storage_account_name = "saspecbuilderdit1"
     container_name       = "sastrycontainer"
     key                  = "sastrytest.tfstate"
-    use_msi              = true
-    subscription_id      = "e495d880-46b4-4223-81c2-6753ed66b43a"
-    tenant_id            = "ff355289-721e-4dd7-a663-afec62ab9d54"
+    access_key           = "vczA8SWz6uhueVyC++ocBsPzyVkjktBx7ShG8BDTSOlpcXlhRpqFpbX3icMswsbXzRHQxBUfKmGB+ASte+vpfQ=="
   }
 }
 
