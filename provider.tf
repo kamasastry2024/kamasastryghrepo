2d62a28f778ffa709011cdd5391c6479dcea5fe8
Kamasastry Kodukulla <Kamasastry.10713580@ltimindtree.com>
2024-02-03 22:07:53 +0530

    added storage account msi

--- provider.tf
+++ provider.tf
@@ -1,10 +1,19 @@
 terraform {
-  required_version = ">= 2.26"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+
   backend "azurerm" {
     resource_group_name  = "rg-spec-builder-eval"
     storage_account_name = "saspecbuilderdit1"
     container_name       = "sastrycontainer"
     key                  = "sastrytest.tfstate"
+    use_msi              = true
+    subscription_id = "e495d880-46b4-4223-81c2-6753ed66b43a"
+    tenant_id       = "ff355289-721e-4dd7-a663-afec62ab9d54"
   }
 }
 
