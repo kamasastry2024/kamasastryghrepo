a7f4970e3b1de9dc17791ab57ae1afec8fca7232
Kamasastry Kodukulla <Kamasastry.10713580@ltimindtree.com>
2024-02-03 21:49:07 +0530

    updated tf version to 155

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.5.7"
+  required_version = ">= 1.5.5"
   backend "azurerm" {
     resource_group_name  = "rg-spec-builder-eval"
     storage_account_name = "saspecbuilderdit1"
